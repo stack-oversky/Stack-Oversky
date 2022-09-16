9da5d2c2e4811cd56f6ae022a6fa5545e2043bb4
이진호 <dlwlsgh0808@gmail.com>
2022-09-16 16:08:01 +0900

    코드 수정
    카메라 이동, 변수 정리, 코드 간결하게 작성함

--- Stack_Over_Sky/Assets/Scripts/이진호/BlockGenerator.cs
+++ Stack_Over_Sky/Assets/Scripts/이진호/BlockGenerator.cs
@@ -5,72 +5,68 @@ using UnityEngine;
 public class BlockGenerator : MonoBehaviour
 {
     // Start is called before the first frame update
-    public GameObject bolckPrefab;      //블럭 프리팹 저장변수
-    GameObject blo1;                    //생성된 idle 블럭 저장용
-    GameObject blo2;                    //위와 같음
-    public float plusHeight = 1;        //블럭 생성 높이를 올리는 변수
-    public float height1 = 3;           //시작 높이
-    public float height2 = 3;           //시작 높이
-    public float max_height = 3;
-    float span = 0.5f;                  //딜레이
-    float delta1 = 0.5f;                //초 세는 변수
-    float delta2 = 0.5f;                //초 세는 변수
-    bool blockExist1 = false;           //블럭 존재 확인
-    bool blockExist2 = false;           //블럭 존재 확인
+    public GameObject bolckPrefab;          //블럭 프리팹 저장변수
+    GameObject saveIdleBlock1;              //생성된 idle 블럭 저장용
+    GameObject saveIdleBlock2;              //위와 같음
+    public float plusHeight = 1;            //블럭 생성 높이를 올리는 변수
+    public Camera cam;                      //카메라의 상대 좌표에서 블럭 생성
+    public float span = 0.5f;               //딜레이
+    float delta1 = 0.5f;                    //초 세는 변수
+    float delta2 = 0.5f;                    //초 세는 변수
+    bool isBlockExist1 = false;             //블럭 존재 확인
+    bool isBlockExist2 = false;             //블럭 존재 확인
 
     // Update is called once per frame
     void Update()
     {
         DeltaTime();//초 세기
         CreateBlock();//블럭 생성
-        max_height = height1 > height2 ? height1 : height2;
     }
 
-    void GetKEYDown1(GameObject b1)//블럭 낙하 함수
+    void BlockDown_L(GameObject b1)//블럭 낙하 함수
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
             delta1 = 0.0f;
-            b1.GetComponent<BlockController>().isDown = true;
-            height1 += plusHeight;
-            blockExist1 = false;
+            b1.GetComponent<BlockController>().isDrop = true;
+            isBlockExist1 = false;
         }
     }
 
-    void GetKEYDown2(GameObject b2)//블럭 낙하 함수 2
+    void BlockDown_R(GameObject b2)//블럭 낙하 함수 2
     {
         if (Input.GetKeyDown(KeyCode.KeypadEnter))
         {
             delta2 = 0.0f;
-            b2.GetComponent<BlockController>().isDown = true;
-            height2 += plusHeight;
-            blockExist2 = false;
+            b2.GetComponent<BlockController>().isDrop = true;
+            isBlockExist2 = false;
         }
     }
 
     void CreateBlock()
     {
-        if (blockExist1 == false && this.span < this.delta1)//블럭이 없을 때 bl1에 BlockController 속성을 가져와서 생성
+        if (isBlockExist1 == false && this.span < this.delta1)//블럭이 없을 때 saveIdleBlock1에 BlockController 속성을 가져와서 생성
         {
-            GameObject bl1 = Instantiate(bolckPrefab) as GameObject;
-            bl1.transform.position = new Vector3(-3, height1, 0);
-            bl1.GetComponent<BlockController>().startPos = new Vector3(-3, height1, 0);
-            blo1 = bl1;
-            blockExist1 = true;
+            GameObject idelBlock = Instantiate(bolckPrefab) as GameObject;
+            idelBlock.transform.position = new Vector3(-3.5f, cam.transform.position.y + 3f, 0);
+            idelBlock.GetComponent<BlockController>().startPos = new Vector3(-3, cam.transform.position.y + 3f, 0);
+            saveIdleBlock1 = idelBlock;
+            isBlockExist1 = true;
         }
-        if (blockExist2 == false && this.span < this.delta2)//블럭이 없을 때 bl2에 BlockController 속성을 가져와서 생성
+        else if (isBlockExist1)//블럭이 있을때 saveIdleBlock 를 전달
+            BlockDown_L(saveIdleBlock1);
+
+        if (isBlockExist2 == false && this.span < this.delta2)//블럭이 없을 때 saveIdleBlock2에 BlockController 속성을 가져와서 생성
         {
-            GameObject bl2 = Instantiate(bolckPrefab) as GameObject;
-            bl2.transform.position = new Vector3(3, height2, 0);
-            bl2.GetComponent<BlockController>().startPos = new Vector3(3, height2, 0);
-            bl2.GetComponent<BlockController>().sign *= -1;
-            blo2 = bl2;
-            blockExist2 = true;
+            GameObject idelBlock = Instantiate(bolckPrefab) as GameObject;
+            idelBlock.transform.position = new Vector3(3.5f, cam.transform.position.y + 3f, 0);
+            idelBlock.GetComponent<BlockController>().startPos = new Vector3(3, cam.transform.position.y + 3f, 0);
+            idelBlock.GetComponent<BlockController>().sign *= -1;
+            saveIdleBlock2 = idelBlock;
+            isBlockExist2 = true;
         }
-        if (blockExist1)//블럭이 있을때 낙하
-            GetKEYDown1(blo1);
-        if (blockExist2)//블럭이 있을때 낙하
-            GetKEYDown2(blo2);
+        else if (isBlockExist2)//블럭이 있을때 saveIdleBlock 를 전달
+            BlockDown_R(saveIdleBlock2);
     }
 
     void DeltaTime()//블럭 생성 딜레이 넣기 위한 코드 현재 0.5초(수정가능)
--- Stack_Over_Sky/Assets/Scripts/이진호/CameraController.cs
+++ Stack_Over_Sky/Assets/Scripts/이진호/CameraController.cs
@@ -5,25 +5,35 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     // Start is called before the first frame update
-    BlockGenerator blockGen;
-
-    public Camera cam;
-    float preHeight;
+    
+    public Camera cam;          //카메라
 
     void Start()
     {
-        blockGen = GameObject.Find("BlockGenerator").GetComponent<BlockGenerator>();
-        preHeight = blockGen.max_height;
     }
 
     // Update is called once per frame
     void Update()
     {
-        //카메라 조정은 아직 미완
-        if (blockGen.max_height > preHeight)
+
+    }
+
+    //Drop 태그인 블럭이 콜라이더에 닿았을때 카메라를 위로 이동
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Drop")
         {
-            preHeight = blockGen.max_height;
-            cam.orthographicSize += 0.3f;
+            //Debug.Log("Enter");
+            cam.transform.position += new Vector3(0, 0.02f, 0);
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.tag == "Drop")
+        {
+            //Debug.Log("Stay");
+            cam.transform.position += new Vector3(0, 0.2f, 0);
         }
     }
 }
