b1cad11b4b38b6d71f889614a32d3a0bce81ba14
gyul <cu29635@naver.com>
2022-09-22 13:58:05 +0900

    코드 주석 달아둠

--- cu29635/stack-over-sky-cu29635/Assets/ShootBlock.cs
+++ cu29635/stack-over-sky-cu29635/Assets/ShootBlock.cs
@@ -2,32 +2,32 @@ using UnityEngine;
 
 public class ShootBlock : MonoBehaviour
 {
-    public GameObject blockFactory;
-    public GameObject blockPosition;
-    public Vector3 start;
-    public float move = 0.5f;
-    public float speed = 0.1f;
+    public GameObject blockFactory; //blockFactoy
+    public GameObject blockPosition; //blockPosition
+    public Vector3 start; //start position
+    public float move = 0.5f; //move
+    public float speed = 0.1f; //
 
-    // Start is called before the first frame update
-
+    //block motion [left to right / drop]
     void BlockMove()
     {
-        if (Input.GetKeyDown(KeyCode.Return))
+        if (Input.GetKeyDown(KeyCode.Return)) //enter key pressed
         {
-            GameObject block = Instantiate(blockFactory);
-            block.transform.position = blockPosition.transform.position;
+            //drop
+            GameObject block = Instantiate(blockFactory); //make block
+            block.transform.position = blockPosition.transform.position; //new block position
         }
-        else
-        {
+        else 
+        {   //block moves left to right
             if (this.transform.position.x > start.x + 3 || this.transform.position.x < start.x - 3) //범위는 시작 지점으로부터 +-2
-                move *= -1;
-            this.transform.position += new Vector3(speed * move, 0, 0);
+                move *= -1; 
+            this.transform.position += new Vector3(speed * move, 0, 0); //block position change
         }
     }
 
     void Start()
     {
-        start = this.transform.position;
+        start = this.transform.position; //block start position;
     }
 
     // Update is called once per frame
