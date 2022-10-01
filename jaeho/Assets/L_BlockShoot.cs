ab25bd84e85c1b5b90ac4a03ecb8afdbfb669f8f
wlrmadlwhgdk <88009937+thswogh@users.noreply.github.com>
2022-10-01 13:11:59 +0900

    StackOverSky-script

---
+++ jaeho/Assets/L_BlockShoot.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class L_BlockShoot: MonoBehaviour
+{
+    public GameObject LblockPrefab; //prefab으로 사용할 L_gameobject변수
+    
+
+    void Start()
+    {
+        
+    }
+
+   
+    void Update()
+    {
+        
+        Vector3 curpos=this.transform.position;  //블록 움직일 때 현위치 저장하는 변수
+
+        if(Input.GetKeyDown(KeyCode.Space)) //Input.GetKey 메소드는 true 값을 반환. 입력받으면 true로 if문 실행
+        { //Input.GetKeyDown은 키를 누르는 순간 True값 한번만 반환.
+          //꾹 누르는 것이 아니라 연타를 해야 True값 여러번 반환.
+            GameObject clone=Instantiate(LblockPrefab);
+            clone.GetComponent<SpriteRenderer>().color=Color.green; //블록 떨어트리면 색깔 변하게
+          
+        }
+
+       // transform.position=curpos;
+        
+    }
+}
