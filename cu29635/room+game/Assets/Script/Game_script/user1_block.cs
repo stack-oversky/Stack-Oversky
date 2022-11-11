e5b2415dd2969f3842603dec89309abe4129ce1f
gyul <dlrbfl2058@naver.com>
2022-11-12 02:05:48 +0900

    1p game 카메라 이동 구현 및 destroy 구현
    1p game,

--- cu29635/room+game/Assets/Script/Game_script/user1_block.cs
+++ cu29635/room+game/Assets/Script/Game_script/user1_block.cs
@@ -8,7 +8,12 @@ public class user1_block : MonoBehaviour
     public GameObject blockPosition;
     public Vector3 start;
     public float move = 0.1f;
-    public float speed = 0.1f;
+    public float speed = 0.05f;
+    public float up = 10;
+    Rigidbody2D rig2d;
+    int k = 1;
+    GameObject block;
+    public int cnt = 0;
 
     // Start is called before the first frame update
 
@@ -16,7 +21,10 @@ public class user1_block : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            GameObject block = Instantiate(blockFactory);
+            block = Instantiate(blockFactory);
+            //rig2d.AddForce(new Vector2(0,speed), ForceMode2D.Force);
+            cnt = cnt + 1;
+            k = 0;
             block.transform.position = blockPosition.transform.position; 
 
         }
@@ -26,12 +34,26 @@ public class user1_block : MonoBehaviour
                 move *= -1;
             this.transform.position -= new Vector3(speed * move, 0, 0);
         }
+        if (cnt > 10&&k==0)
+        {
+            //this.transform.position += new Vector3(0, 1, 0);
+            for(int i = 0; i < 100; i++)
+            {
+                this.transform.position += new Vector3(0, up * Time.deltaTime, 0);
+            }
+            k = 1;
+            
+            //rig2d.AddForce(new Vector2(start.x,1), ForceMode2D.Force);
+
+        }
+
     }
    
 
+
     void Start()
     {
-        start = this.transform.position;
+        rig2d = gameObject.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
