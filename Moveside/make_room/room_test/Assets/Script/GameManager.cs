475bd7ed60599435205e0061fd7618a6a58f0d9c
moveside <jekker77@gmail.com>
2022-12-01 01:39:33 +0900

    feat : Game2 ver1 완성

--- Moveside/make_room/room_test/Assets/Script/GameManager.cs
+++ Moveside/make_room/room_test/Assets/Script/GameManager.cs
@@ -12,17 +12,24 @@ public class GameManager : MonoBehaviour
     public GameObject block_L;
     public GameObject block_R;
     public GameObject blockPrefab;
+    public GameObject blockPrefab2;
+
     public GameObject seesaw;
+    public GameObject[] itemSprite1 = new GameObject[4];
+    public GameObject[] itemSprite2 = new GameObject[4];
+    public GameObject[] statusPlayer1 = new GameObject[4];
+    public GameObject[] statusPlayer2 = new GameObject[4];
 
     public TMP_Text uiText;
     public TMP_Text scoreText;
     public TMP_Text timeText;
-    public float score = 0;
+    public float score;
 
     //시간 관련 변수
     public float spawnSpan = 0.5f;//생성 딜레이
     public float delaySpan = 0.1f;//움직임 딜레이
     public float itemSpan = 3f;//아이템 지속시간(3초)
+
     public float itemTime = 5f;
     //움직임 딜레이
     float delayDelta1 = 2f;
@@ -48,6 +55,8 @@ public class GameManager : MonoBehaviour
     public Camera otherCam;
 
     public List<GameObject> blocks = new List<GameObject>();
+    public List<GameObject> blocks2 = new List<GameObject>();
+
     public List<GameObject> droppedBlocks = new List<GameObject>();
 
     public GameObject resultPanel;
@@ -57,11 +66,31 @@ public class GameManager : MonoBehaviour
     public TMP_Text player2Name;
 
     bool isGame = false;
+
+
+
+    private float itemSpan1 = 0f;
+    private float stopSpan1 = 0f;
+    private float fastSpan1 = 0f;
+    private float slowSpan1 = 0f;
+    private bool isStop1 = false;
+
+    private float itemSpan2 = 0f;
+    private float stopSpan2 = 0f;
+    private float fastSpan2 = 0f;
+    private float slowSpan2 = 0f;
+    private bool isStop2 = false;
+
+    private float blockY;
+
+    public bool goLobby = false;
+    public GameObject endButton;
     // Start is called before the first frame update
     void Start()
     {
         StartCoroutine(ReadyGame());
         StartCoroutine(CalScore());
+        StartCoroutine(CameraMove());
     }
 
     // Update is called once per frame
@@ -71,7 +100,22 @@ public class GameManager : MonoBehaviour
         Create();
         UseItem();
         TimeCount();
-        CalScore();
+    }
+    public IEnumerator CameraMove()
+    {
+        while (true)
+        {
+            if (cam.transform.position.y < blockY + 1f)
+            {
+                cam.transform.position += new Vector3(0, Time.deltaTime, 0);
+            }
+            if (cam.transform.position.y > blockY - 1f)
+            {
+                cam.transform.position -= new Vector3(0, Time.deltaTime, 0);
+
+            }
+            yield return new WaitForEndOfFrame();
+        }
     }
     public IEnumerator CalScore()
     {
@@ -79,14 +123,20 @@ public class GameManager : MonoBehaviour
         {
             for (int i = 0; i < droppedBlocks.Count; i++)
             {
-                if (score < droppedBlocks[i].gameObject.transform.position.y + 3)
+                float maxY = 0;
+                if (maxY < droppedBlocks[i].gameObject.transform.position.y + 3)
                 {
-                    float currentScore = droppedBlocks[i].gameObject.transform.position.y + 3;
-                    currentScore = Mathf.Round(currentScore * 100) * 0.01f;
-                    StartCoroutine(SetScore(currentScore));
+                    maxY = droppedBlocks[i].gameObject.transform.position.y + 3;
                 }
+                if (maxY > score)
+                {
+                    maxY = Mathf.Round(maxY * 100) * 0.01f;
+                    StartCoroutine(SetScore(maxY));
+                }
+                blockY = maxY;
             }
-            yield return new WaitForSeconds(1f);
+
+            yield return new WaitForSeconds(0.3f);
         }
     }
     public IEnumerator SetScore(float currentScore)
@@ -100,11 +150,11 @@ public class GameManager : MonoBehaviour
 
             score += offset * Time.fixedDeltaTime;
             score = Mathf.Round(score * 100) * 0.01f;
-            scoreText.text = "Score : " + score.ToString() + "m";
+            scoreText.text = score.ToString() + "m";
             yield return null;
         }
         score = currentScore;
-        scoreText.text = "Score : " + score.ToString() + "m";
+        scoreText.text = score.ToString() + "m";
     }
 
     public IEnumerator ReadyGame()
@@ -121,7 +171,8 @@ public class GameManager : MonoBehaviour
         yield return new WaitForSecondsRealtime(1f);
         uiText.text = "";
         isGame = true;
-    }
+        score = 0;
+}
     void Move()
     {
         if(block_L.transform.position.x < -6.5)     sign1 = 1;
@@ -150,13 +201,14 @@ public class GameManager : MonoBehaviour
             {
                 newBlock.transform.localScale *= 1.4f;
                 bigTrigger1 = false;
+                statusPlayer1[1].gameObject.SetActive(false);
             }
             delayDelta1 = 0f;
             blocks.Add(newBlock);
         }
         if(Input.GetKeyDown(KeyCode.KeypadEnter) && delta2 > spawnSpan)
         {
-            GameObject newBlock = Instantiate(blockPrefab) as GameObject;
+            GameObject newBlock = Instantiate(blockPrefab2) as GameObject;
             newBlock.transform.position = posR;
             newBlock.tag = "Dropping";
             delta2 = 0f;
@@ -164,9 +216,10 @@ public class GameManager : MonoBehaviour
             {
                 newBlock.transform.localScale *= 1.4f;
                 bigTrigger2 = false;
+                statusPlayer2[1].gameObject.SetActive(false);
             }
             delayDelta2 = 0f;
-            blocks.Add(newBlock);
+            blocks2.Add(newBlock);
             
         }
     }
@@ -178,6 +231,64 @@ public class GameManager : MonoBehaviour
         delayDelta1 += Time.deltaTime;
         delayDelta2 += Time.deltaTime;
         itemSpan -= Time.deltaTime;
+        // player1 time count
+        if(stopSpan1>0)
+        {
+            stopSpan1 -= Time.deltaTime;
+            if(stopSpan1 <= 0)
+            {
+                moveSpeed = 4f;
+                statusPlayer1[0].SetActive(false);
+            }
+        }
+        if(fastSpan1>0)
+        {
+            fastSpan1 -= Time.deltaTime;
+            if(fastSpan1 <=0)
+            {
+                moveSpeed = 4f;
+                statusPlayer1[2].SetActive(false);
+            }
+        }
+        if(slowSpan1>0)
+        {
+            slowSpan1 -= Time.deltaTime;
+            if(slowSpan1 <=0)
+            {
+                moveSpeed = 4f;
+                statusPlayer1[3].SetActive(false);
+            }
+        }
+        // player2 time count
+        if (stopSpan2>0)
+        {
+            stopSpan2 -= Time.deltaTime;
+            if (stopSpan2 <= 0)
+            {
+                moveSpeed2 = 4f;
+                statusPlayer2[0].SetActive(false);
+            }
+        }
+        if(fastSpan2>0)
+        {
+            fastSpan2 -= Time.deltaTime;
+            if(fastSpan2<=0)
+            {
+                moveSpeed2 = 4f;
+                statusPlayer2[2].SetActive(false);
+            }
+        }
+        if(slowSpan2>0)
+        {
+            slowSpan2 -= Time.deltaTime;
+            if(slowSpan2<=0)
+            {
+                moveSpeed2 = 4f;
+                statusPlayer2[3].SetActive(false);
+            }
+        }
+
+
         if (isGame)
         {
             gameTime -= Time.deltaTime;
@@ -187,51 +298,48 @@ public class GameManager : MonoBehaviour
 
     void UseItem()
     {
-        if (slowTrigger)
-            moveSpeed = 2f;
-        else if (fastTrigger)
-            moveSpeed = 10f;
-        if(itemSpan < 0)
-        {
-            fastTrigger = false;
-            slowTrigger = false;
-            moveSpeed = 4f;
-            moveSpeed2 = 4f;
-        }
     }
     public void stopBlock(int playerIndex)
     {
         if(playerIndex == 1)
         {
             moveSpeed = 0f;
+            stopSpan1 = itemTime;
+            statusPlayer1[0].SetActive(true);
         }
         else if(playerIndex == 2)
         {
             moveSpeed2 = 0f;
+            stopSpan2 = itemTime;
+            statusPlayer2[0].SetActive(true);
         }
-        itemSpan = itemTime;
     }
     public void fastBlock(int playerIndex)
     {
-        if(playerIndex == 1) 
+        if(playerIndex == 1)
         {
             moveSpeed = 10f;
+            fastSpan1 = itemTime;
+            statusPlayer1[2].SetActive(true);
         }
-        else if (playerIndex == 2)
+        else if(playerIndex == 2)
         {
             moveSpeed2 = 10f;
+            fastSpan2 = itemTime;
+            statusPlayer2[2].SetActive(true);
         }
-        itemSpan = itemTime;
     }
     public void bigBlock(int playerIndex)
     {
         if (playerIndex == 1)
         {
             bigTrigger1 = true;
+            statusPlayer1[1].gameObject.SetActive(true);
         }
         else if (playerIndex == 2)
         {
             bigTrigger2 = true;
+            statusPlayer2[1].gameObject.SetActive(true);
         }
     }
     public void slowBlock(int playerIndex)
@@ -239,16 +347,20 @@ public class GameManager : MonoBehaviour
         if (playerIndex == 1)
         {
             moveSpeed = 2f;
+            slowSpan1 = itemTime;
+            statusPlayer1[3].SetActive(true);
         }
         else if (playerIndex == 2)
         {
             moveSpeed2 = 2f;
+            slowSpan2 = itemTime;
+            statusPlayer2[3].SetActive(true);
         }
-        itemSpan = itemTime;
     }
     public void GameEnd(string score1,string score2,string player1,string player2)
     {
         resultPanel.SetActive(true);
+        endButton.SetActive(false);
         StartCoroutine(CountScore(score1, score2));
         player1Name.text = player1;
         player2Name.text = player2;
@@ -322,12 +434,12 @@ public class GameManager : MonoBehaviour
             player2Score.text = currentScore2.ToString("F2") + "m";
             yield return null;
         }
-        while(currentScore1 <= player1ScoreFloat)
+        while(currentScore1 < player1ScoreFloat)
         {
             currentScore1 += offset1 * Time.fixedDeltaTime;
             player1Score.text = currentScore1.ToString("F2") + "m";
         }
-        while (currentScore2 <= player2ScoreFloat)
+        while (currentScore2 < player2ScoreFloat)
         {
             currentScore2 += offset2 * Time.fixedDeltaTime;
             player2Score.text = currentScore2.ToString("F2") + "m";
@@ -336,5 +448,10 @@ public class GameManager : MonoBehaviour
         currentScore2 = player2ScoreFloat;
         player1Score.text = currentScore1.ToString("F2") + "m";
         player2Score.text = currentScore2.ToString("F2") + "m";
+        endButton.SetActive(true);
+    }
+    public void EndButton()
+    {
+        Application.Quit();
     }
 }
